64ee2d4b6a26ca778f176c6e02e5be6d31ba6ef7
xiao bo <840363720@qq.com>
2019-05-28 20:23:59 +0800

    like表数据迁移

---
+++ database/migrations/2019_05_28_095447_create_likes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->comment('点赞人的id');
+            $table->integer('topic_id')->comment('点赞的文章id');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
