0ca4a3ff217282ab3408478893e7b5d6e571555f
xiao bo <840363720@qq.com>
2019-06-09 19:58:34 +0800

    个人中心页面修改

--- resources/views/home/info/index.blade.php
+++ resources/views/home/info/index.blade.php
@@ -1,38 +1,50 @@
 @extends('layouts.app')
 @section('content')
     <div class="row">
-        <div class="col-sm-9">
-            <div class="card" >
+        @include('layouts/info_left')
+        <div class="col-sm-7">
+            <div class="card">
+                <ul class="list-group list-group-flush">
+                    @foreach($topics as $topic)
+                        <li class="list-group-item">
+                            <div class="media">
+                                <img class="mr-3" src="{{ optional($topic->user)->avatar }}" alt="Generic placeholder image">
+                                <div class="media-body">
+                                    <span class="" style="float:right; margin-top:10px;font-size:14px;"><a href="{{ route('home.info.destroy', ['id'=>$topic->id]) }}">删除</a></span>
+                                    <span style="float:right; margin-top:10px;font-size:14px;"><a href="{{ route('home.info.edit', ['id'=>$topic->id]) }}">编辑</a>&nbsp;&nbsp;</span>
+                                    <h6 class="mt-0"><a href="{{ route('home.topic.index', ['id'=>$topic->id]) }}">{{ $topic->title }}</a></h6>
+                                    <span class="post-cate"><a href="">{{ optional($topic->nodes)->name }}</a></span>
+                                    <span class="post-cate"><a href="">{{ optional($topic->user)->name }}</a></span>
+                                    <span class="post-cate">{{ $topic->created_at }}</span>
+                                </div>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div class="card">
                 <div class="card-body">
-                    <div class="setting-border">
-                        <h5 class="card-title">发布的文章</h5>
+                    <img class="mr-3" src="{{ optional($topic->user)->avatar }}" alt="加载失败" width=100% height=100%>
+                    <div class="info-div">
+                            <span class="info-span">话题</span>
+                            <span class="info-span">粉丝</span>
+                            <span class="info-span">喜欢</span>
+                            <span class="info-span">15820</span>
+                            <span class="info-span">8520</span>
+                            <span class="info-span">96250</span>
+
                     </div>
-                    <div class="row">
-                        <div class="col-sm-12" style="margin-top: 20px;">
-                            <ul class="list-group list-group-flush">
-                                @foreach($topics as $topic)
-                                <li class="list-group-item">
-                                    <div class="media">
-                                        <img class="mr-3" src="http://cdn.guanggoo.com//static/avatar/37/m_default.png" alt="Generic placeholder image">
-                                        <div class="media-body">
-                                            <span class="" style="float:right; margin-top:10px;font-size:14px;"><a href="{{ route('home.info.destroy', ['id'=>$topic->id]) }}">删除</a></span>
-                                            <span style="float:right; margin-top:10px;font-size:14px;"><a href="{{ route('home.info.edit', ['id'=>$topic->id]) }}">编辑</a>&nbsp;&nbsp;</span>
-                                            <h6 class="mt-0"><a href="{{ route('home.topic.index', ['id'=>$topic->id]) }}">{{ $topic->title }}</a></h6>
-                                            {{--@foreach($topic->group as $group)--}}
-                                            <span class="post-cate"><a href="">{{ optional($topic->nodes)->name }}</a></span>
-                                            {{--@endforeach--}}
-                                            <span class="post-cate"><a href="">{{ optional($topic->user)->name }}</a></span>
-                                            <span class="post-cate">{{ $topic->created_at }}</span>
-                                        </div>
-                                    </div>
-                                </li>
-                                    @endforeach
-                            </ul>
-                        </div>
+
+                    <div class="info-div">.
+                        <a href="" type="button" class=" btn-light btn-block">关注</a>
                     </div>
+
                 </div>
             </div>
         </div>
-        @include('layouts/info')
+
     </div>
 @endsection
---
+++ resources/views/layouts/info_left.blade.php
@@ -0,0 +1,24 @@
+<div class="col-sm-2">
+    <div class="card">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><b>我的创作</b></li>
+            <li class="list-group-item"><a href="{{ route('home.info.index') }}">我的话题</a></li>
+        </ul>
+    </div>
+    <div class="card" style="margin-top:15px;">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><b>我的互动</b></li>
+            <li class="list-group-item"><a href="">我的回复</a></li>
+            <li class="list-group-item"><a href="">我的收藏</a></li>
+            <li class="list-group-item"><a href="">我的点赞</a></li>
+        </ul>
+    </div>
+
+    <div class="card" style="margin-top:15px;">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><b>我的关系</b></li>
+            <li class="list-group-item"><a href="">我的关注</a></li>
+            <li class="list-group-item"><a href="">我的粉丝</a></li>
+        </ul>
+    </div>
+</div>
