2a8b01ea29166342ec9d43ffcfcb9b65ec779375
xiao bo <840363720@qq.com>
2019-06-11 22:07:12 +0800

    后端首页

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -1,5 +1,44 @@
 @extends('layouts.backend')
 @section('content')
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="card" style="margin-top: 10px;">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">用户</li>
+                    <li class="list-group-item">
+                        <div>今日注册：20</div>
+                        <div>今日注册：20</div>
+                        <div>今日注册：20</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
 
+        <div class="col-sm-4">
+            <div class="card" style="margin-top: 10px;">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">话题</li>
+                    <li class="list-group-item">
+                        <div>今日发表：20</div>
+                        <div>今日发表：20</div>
+                        <div>今日发表：20</div>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card" style="margin-top: 10px;">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">回复</li>
+                    <li class="list-group-item">
+                        <div>今日回复：20</div>
+                        <div>今日回复：20</div>
+                        <div>今日回复：20</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 @endsection
